234acf222a3ce7d9fa86a6135eb682b6d3ce72b2
asinghwiley <asingh@wiley.com>
2021-12-31 14:29:49 +0530

    added p

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use("/api", productRoutes)
 app.use("/api", CategoryRoutes)
 app.use("/api", OwnerRoutes)
 
-const port = Process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, (err) => {
   if(err) {
